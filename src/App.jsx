da80e580b63e3f5d841a5bb88c5dc32884a25536
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-21 23:32:52 +0530

    feat: react router added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import GuidePage from "./pages/GuidePage";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <GuidePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/GuidePage" element={<GuidePage />} />
+      </Routes>
       <Footer />
     </>
   );
