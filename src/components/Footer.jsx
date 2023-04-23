be888eafd475753334ff61d09c39e402f66128a6
Saurav Pant <pantsaurav005@gmail.com>
2023-04-23 07:00:37 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer aria-label="Site Footer" className="bg-white dark:bg-gray-900">
+    <footer aria-label="Site Footer" className="bg-gray-900">
       <div className="max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-white sm:text-5xl">
@@ -16,7 +16,7 @@ export default function Footer() {
           </p>
         </div>
 
-        <div className="pt-8 mt-16 border-t flex gap-3 border-gray-100 dark:border-gray-800   sm:justify-between lg:mt-24">
+        <div className="pt-8 mt-16 border-t flex gap-3 border-gray-800   sm:justify-between lg:mt-24">
           <a
             href="https://github.com/gabrysia694/Gym-Junkies/blob/main/LICENSE"
             target="_blank"
@@ -30,7 +30,6 @@ export default function Footer() {
             rel="noreferrer"
             target="_blank"
             className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
-            aria-label="GitHub repository for Gym Junkies"
           >
             <FaGithub className="text-2xl" />
           </a>
