e08526733a2727f4768c434bb72d5fbb15018daf
Saurav Pant <pantsaurav005@gmail.com>
2023-04-24 19:11:34 +0530

    Theme Persist after Refreshing

--- src/context/theme.jsx
+++ src/context/theme.jsx
@@ -1,44 +1,53 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react"
 
-const ThemeContext = createContext("light");
+const ThemeContext = createContext("light")
 
 const lightTheme = {
   background: "#fff",
   color: "#000",
+  icon: "☀️",
   button: {
-    background: "#000",
-    color: "#fff",
+    buttonBgColor: "#000",
+    buttonTextColor: "#fff",
   },
-  icon:'☀️'
-};
+  navbar: {
+    background: "#fff",
+    color: "#000",
+  },
+}
 
 const darkTheme = {
   background: "#000",
   color: "#fff",
+  icon: "🌙",
   button: {
-    background: "#fff",
-    color: "#000",
+    buttonBgColor: "#fff",
+    buttonTextColor: "#000",
   },
-  icon:'🌙',
-};
-
+  navbar: {
+    background: "#111827",
+    color: "#fff",
+  },
+}
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(lightTheme);
+  const [theme, setTheme] = useState(lightTheme)
 
   const toggleTheme = () => {
-    if (theme === lightTheme) {
-      setTheme(darkTheme);
-    } else {
-      setTheme(lightTheme);
+    const newTheme = theme === lightTheme ? darkTheme : lightTheme
+    setTheme(newTheme)
+    localStorage.setItem("selectedTheme", JSON.stringify(newTheme))
+  }
+
+  // Retrieve theme from local storage, if available
+  useEffect(() => {
+    const savedTheme = JSON.parse(localStorage.getItem("selectedTheme"))
+    if (savedTheme) {
+      setTheme(savedTheme)
     }
-  };
+  }, [])
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>
+}
 
-export { ThemeProvider, ThemeContext };
+export { ThemeProvider, ThemeContext }
