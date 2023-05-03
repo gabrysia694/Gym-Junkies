ec93f8c420a28935bf935d9ebfd68121a7fc36ac
Lalman <chauhanlakshay074@gmail.com>
2023-05-03 19:45:18 +0530

    github api added

--- src/components/contributors/ContributorCard.jsx
+++ src/components/contributors/ContributorCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const ContributorCard = ({ name, github, photo }) => {
+const ContributorCard = ({ login, html_url, avatar_url }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow">
       <div className="flex items-center mb-4">
-        <img src={photo} alt={name} className="w-12 h-12 rounded-full" />
+        <img src={avatar_url} alt={login} className="w-12 h-12 rounded-full" />
         <div className="ml-4">
-          <h2 className="text-lg font-medium">{name}</h2>
+          <h2 className="text-lg font-medium">{login}</h2>
           <a
-            href={`https://github.com/${github}`}
+            href={html_url}
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-500 hover:underline"
           >
-            {github}
+            {login}
           </a>
         </div>
       </div>
