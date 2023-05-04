7f5b052f3a219102a3d377dfc0f9289856e47dac
Lalman <chauhanlakshay074@gmail.com>
2023-05-04 23:19:58 +0530

    dark mode card color fix

--- src/components/contributors/ContributorCard.jsx
+++ src/components/contributors/ContributorCard.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React,{useContext} from "react";
+import { ThemeContext } from "../../context/theme";
+import clsx from "clsx";
 
 const ContributorCard = ({ login, html_url, avatar_url }) => {
+  const { themeName } = useContext(ThemeContext);
+  console.log(themeName);
   return (
-    <div className="bg-white p-4 rounded-lg shadow">
+    <div className=
+    {clsx(
+      " p-4 rounded-lg shadow",
+      themeName === "light" ? "text-white bg-gray-800" : "text-black bg-white"
+    )}
+    >
       <div className="flex items-center mb-4">
         <img src={avatar_url} alt={login} className="w-12 h-12 rounded-full" />
         <div className="ml-4">
@@ -13,7 +22,7 @@ const ContributorCard = ({ login, html_url, avatar_url }) => {
             rel="noopener noreferrer"
             className="text-blue-500 hover:underline"
           >
-            {login}
+            View Profile
           </a>
         </div>
       </div>
