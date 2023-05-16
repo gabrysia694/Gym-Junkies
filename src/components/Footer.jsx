7c3ee3bd7d49a317ab43a3fb0599bf476172936f
Jayraj singh <97652040+JayrajSingh65@users.noreply.github.com>
2023-05-17 01:26:04 +0530

    improved ui and user Accessibility for better ux (#143)
    Co-authored-by: jayrajsingh <singhjayraj@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer aria-label="Site Footer" className="bg-gray-900">
-      <div className="max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24">
+    <footer aria-label="Site Footer" className="bg-gray-900 h-auto">
+      <div className="max-width-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-white sm:text-5xl">
             Proudly Open Source 🍕
