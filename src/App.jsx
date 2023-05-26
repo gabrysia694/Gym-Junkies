5d76f27d77d4de0b6232811794f982b459f4923c
aashay shah <aashayshah28@gmail.com>
2023-05-26 17:34:45 +0530

    Adding a weekly schedule for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import GuidePage from "./pages/GuidePage";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import DocsPage from "./pages/DocsPage";
-import AddExercisePage from "./pages/AddExercisePage";
-import { ThemeContext } from "./context/theme";
-import PageNotFound from "./components/PageNotFound";
-import ContributorsPage from "./pages/ContributorsPage";
-
+import React, { useContext } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import GuidePage from './pages/GuidePage';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DocsPage from './pages/DocsPage';
+import AddExercisePage from './pages/AddExercisePage';
+import { ThemeContext } from './context/theme';
+import PageNotFound from './components/PageNotFound';
+import ContributorsPage from './pages/ContributorsPage';
+import SchedulePage from './pages/SchedulePage';
 
 function App() {
   const { theme } = useContext(ThemeContext);
@@ -21,15 +21,17 @@ function App() {
         color: theme.color,
         button: theme.button,
         link: theme.link,
-      }}>
+      }}
+    >
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/GuidePage" element={<GuidePage />} />
-        <Route path="/DocsPage" element={<DocsPage />} />
-        <Route path="/ContributorsPage" element={<ContributorsPage />} />
-        <Route path="/AddExercisePage" element={<AddExercisePage />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/GuidePage' element={<GuidePage />} />
+        <Route path='/SchedulePage' element={<SchedulePage />} />
+        <Route path='/DocsPage' element={<DocsPage />} />
+        <Route path='/ContributorsPage' element={<ContributorsPage />} />
+        <Route path='/AddExercisePage' element={<AddExercisePage />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../context/theme";
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
+import React, { useContext } from 'react';
+import { ThemeContext } from '../context/theme';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
 
 function navLinkClass(isActive) {
-  return clsx("rounded-lg p-1 px-3 py-2 hover:bg-slate-400", {
-    "bg-slate-400": isActive,
+  return clsx('rounded-lg p-1 px-3 py-2 hover:bg-slate-400', {
+    'bg-slate-400': isActive,
   });
 }
 
@@ -13,22 +13,22 @@ export default function Navbar() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <nav
-      aria-label="Site Nav"
-      className="flex items-center justify-between max-w-3xl p-4 mx-auto"
+      aria-label='Site Nav'
+      className='flex items-center justify-between max-w-3xl p-4 mx-auto'
     >
-      <a href="/">
+      <a href='/'>
         <img
-          src="https://user-images.githubusercontent.com/88102392/233238344-b05e3c5d-178e-4a7b-9757-60063fb0f969.png"
-          className="inline-flex h-[1] w-10 items-center justify-center rounded-lg" // don't change logo's height and width here
-          alt="Gym Junkies logo"
-          loading="lazy"
+          src='https://user-images.githubusercontent.com/88102392/233238344-b05e3c5d-178e-4a7b-9757-60063fb0f969.png'
+          className='inline-flex h-[1] w-10 items-center justify-center rounded-lg' // don't change logo's height and width here
+          alt='Gym Junkies logo'
+          loading='lazy'
         ></img>
       </a>
 
-      <ul className="flex flex-wrap items-center gap-2 text-[1rem]">
+      <ul className='flex flex-wrap items-center gap-2 text-[1rem]'>
         <li>
           <NavLink
-            to="/GuidePage"
+            to='/GuidePage'
             className={({ isActive }) => navLinkClass(isActive)}
           >
             Guide
@@ -36,8 +36,16 @@ export default function Navbar() {
         </li>
         <li>
           <NavLink
+            to='/SchedulePage'
             className={({ isActive }) => navLinkClass(isActive)}
-            to="/DocsPage"
+          >
+            Schedule
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => navLinkClass(isActive)}
+            to='/DocsPage'
           >
             Docs
           </NavLink>
@@ -45,13 +53,13 @@ export default function Navbar() {
         <li>
           <NavLink
             className={({ isActive }) => navLinkClass(isActive)}
-            to="/ContributorsPage"
+            to='/ContributorsPage'
           >
             Contributors
           </NavLink>
         </li>
       </ul>
-      <button onClick={toggleTheme} className="text-2xl">
+      <button onClick={toggleTheme} className='text-2xl'>
         {theme.icon}
       </button>
     </nav>
