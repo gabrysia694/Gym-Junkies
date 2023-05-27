378e932676c88933d367fec7fcfcebcd6d2e32cf
Shiva Sai <shivas.bojjalwar@gmail.com>
2023-05-27 15:22:12 +0530

    Update GuidPage (#161)

--- src/pages/GuidePage.jsx
+++ src/pages/GuidePage.jsx
@@ -7,10 +7,10 @@ export default function GuidePage() {
       <div className="flex flex-col mx-auto max-w-screen-xl p-5">
         <h2 className="text-3xl font-bold sm:text-4xl">Gym Junkies</h2>
         <p className="mt-4 ">
-          Gym Junkies is a community of people who love to workout. We are a
-          group of people who are passionate about fitness and want to share our
-          knowledge with others. We are a community of people who are passionate
-          about fitness and want to share our knowledge with others.
+        Gym Junkies is an enthusiastic community comprised of individuals 
+        who have a profound love for physical fitness.We are a collective of passionate
+        individuals driven by a desire to share our extensive knowledge and 
+        expertise with others, fostering a community focused on promoting wellness and vitality.
         </p>
       </div>
       <Card />
