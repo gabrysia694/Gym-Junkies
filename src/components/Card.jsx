c8c73dc4238d31c227e00fadcfc9234b01710ce2
Manuel <manuelpinedacabeza@gmail.com>
2023-06-18 00:43:28 +0200

    Exercise Cards and Schedule Pages with Night Mode (#193)
    * Added github link to contribution text.
    
    Also added white text and underline on hover
    to  link.
    
    * Added conditional styling depending on themeName to
    
    exercise card title, text, image, watch video text,
    added by text and contributor name text.
    
    Made exercise title italic.
    
    * Added conditional styling depending on themeName
    
    to schedule card, title and text.
    
    Made schedule card text italic.
    
    * Removed console log
    
    * Added p-4 to both image and text sections of card.
    
    Added w-4/6 to card by default, added margin auto to center card,
    added w-3/6 to card from medium screens on.
    
    Added w-full to text section by default, added w-3/6 to
    text sections from medium screens on.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,48 +1,88 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
+
 import jsonData from "../DB/exerciseData.json";
+import clsx from "clsx";
 
 export default function Card() {
+  const { themeName } = useContext(ThemeContext);
+
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 p-3">
+    <div className='grid grid-cols-1 lg:grid-cols-2 p-3'>
       {jsonData.map((exercise, index) => (
         <div
           key={index}
-          className=" max-w-md mx-auto my-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:border-purple-500 border-2 px-5"
+          className={clsx(
+            "max-w-md mx-auto my-4 rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:border-purple-500 border-2",
+            themeName === "light" ? "bg-gray-800" : "bg-white"
+          )}
         >
-          <div className="md:flex items-center  ">
-            <img
-              className="w-64 object-cover h-full mx-auto md:w-48"
-              loading="lazy"
-              src={exercise.image}
-              alt={exercise.exercise}
-            />
-            <div className="p-8">
-              <div className="uppercase tracking-wide text-sm text-indigo-800 font-semibold">
+          <div className='md:flex items-center'>
+            <div className='p-4  w-4/6 mx-auto md:w-3/6'>
+              <img
+                className={clsx(
+                  "object-cover h-full mx-auto md:w-full bg-white border-2",
+                  themeName === "light"
+                    ? "border-indigo-400 rounded-xl"
+                    : "border-white"
+                )}
+                loading='lazy'
+                src={exercise.image}
+                alt={exercise.exercise}
+              />
+            </div>
+
+            <div className='p-4 w-full md:w-3/6'>
+              <div
+                className={clsx(
+                  "uppercase italic tracking-wide text-xl font-semibold",
+                  themeName === "light" ? "text-indigo-400" : "text-indigo-800"
+                )}
+              >
                 {exercise.exercise}
               </div>
               <div style={{ overflowY: "auto", height: "30vh" }}>
-                <ul className="p-2 text-gray-500 list-disc">
+                <ul
+                  className={clsx(
+                    "p-2 list-dis",
+                    themeName === "light" ? "text-white-500" : "text-gray-500"
+                  )}
+                >
                   {exercise.instructions.map((instruction, index) => (
                     <li key={index}>{instruction}</li>
                   ))}
                 </ul>
               </div>
-              <div className="mt-6 gap-5 flex items-center">
+              <div className='mt-6 gap-5 flex items-center'>
                 <a
                   href={exercise.videoLink}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-indigo-800 hover:text-indigo-600 font-semibold"
+                  target='_blank'
+                  rel='noreferrer'
+                  className={clsx(
+                    "hover:text-indigo-600 font-semibold",
+                    themeName === "light"
+                      ? "text-indigo-400"
+                      : "text-indigo-800"
+                  )}
                 >
                   Watch video
                 </a>
-                <span className="text-black">
+                <span
+                  className={clsx(
+                    themeName === "light" ? "text-white" : "text-black"
+                  )}
+                >
                   Added by :{" "}
                   <a
                     href={`https://github.com/${exercise["gh-name"]}`}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-indigo-800 hover:text-indigo-600 font-semibold"
+                    target='_blank'
+                    rel='noreferrer'
+                    className={clsx(
+                      themeName === "light"
+                        ? "text-indigo-400"
+                        : "text-indigo-800",
+                      "hover:text-indigo-600 font-semibol"
+                    )}
                   >
                     {exercise["gh-name"]}
                   </a>
