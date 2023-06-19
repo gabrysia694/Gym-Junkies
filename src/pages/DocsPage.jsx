b48b122b5801c5fb725cc6b671a710f68aea8c1d
Shiva-Sai-ssb <shivas.bojjalwar@gmail.com>
2023-06-19 11:50:13 +0530

    Update DocsPage.jsx

--- src/pages/DocsPage.jsx
+++ src/pages/DocsPage.jsx
@@ -10,33 +10,6 @@ export default function DocsPage() {
 }`;
   return (
     <section className="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-      {/* For Grid Background  */}
-      <div className="absolute inset-0 -z-10 overflow-hidden">
-        <svg
-          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-700 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-              width={200}
-              height={200}
-              x="50%"
-              y={-1}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d="M100 200V.5M.5 .5H200" fill="none" />
-            </pattern>
-          </defs>
-          <rect
-            width="100%"
-            height="100%"
-            strokeWidth={0}
-            fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
-          />
-        </svg>
-      </div>
-
       <article className="grid justify-center items-center max-w-5xl mx-auto ">
         <div className="  text-base leading-7">
           <p className="mb-5 text-xl font-bold tracking-tight">
